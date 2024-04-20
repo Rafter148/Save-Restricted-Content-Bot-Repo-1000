0d57cf74a2664e39005130002964e1f7fb07135b
Rafter148 <165582023+Rafter148@users.noreply.github.com>
2024-04-20 11:56:44 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,4 @@
-#Join me at telegram @hemendra148
+#Join me at telegram @hemendraa148
 
 from pyrogram import Client
 
@@ -39,7 +39,7 @@ userbot = Client("myacc",api_id=API_ID,api_hash=API_HASH,session_string=SESSION)
 try:
     userbot.start()
 except BaseException:
-    print("Your session expired please re add that... thanks @hemendra148.")
+    print("Your session expired please re add that... thanks @hemendraa148.")
     sys.exit(1)
 
 Bot = Client(
